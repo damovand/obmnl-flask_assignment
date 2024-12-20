0e1c53761659df9a0478338b51ef50bfdf66d5c7
Leila <damovand@yahoo.com>
2024-12-20 11:49:58 -0500

    CRUD with Flask - getting started

--- app.py
+++ app.py
@@ -37,11 +37,33 @@ def add_transaction():
    # If the request method is GET, render the form template to display the add transaction form
     return render_template("form.html")
 
+# Update operation: Display edit transaction form
+# Route to handle the editing of an existing transaction
+@app.route("/edit/<int:transaction_id>", mthods=["GET","POST"]):
+def edit_transaction(transaction_id):
+    # Check if the request method is POST (form submission)
+    if request.method == 'POST':
+        # Extract the updated values from the form fields
+        
+        date = request.form['date']           # Get the 'date' field value from the form
+        amount = float(request.form['amount'])# Get the 'amount' field value from the form and convert it to a float
 
+        for transaction in transactions:
+         if transaction['id'] == transaction_id:
+                transaction['date'] = date       # Update the 'date' field of the transaction
+                transaction['amount'] = amount   # Update the 'amount' field of the transaction
+                break                            # Exit the loop once the transaction is found and updated
+        # Redirect to the transactions list page after adding the new transaction
+        return redirect(url_for("get_transactions"))
 
-# Update operation
-
-# Delete operation
-
+   # If the request method is GET, render the form template to display the add transaction form
+    return render_template("form.html")
+# Defor transaction in transactions:
+        if transaction['id'] == transaction_id:
+            # Render the edit form template and pass the transaction to be edited
+            return render_template("edit.html", transaction=transaction)
+   # If the transaction with the specified ID is not found, handle this case (optional)
+    return {"message": "Transaction not found"}, 404
+    
 # Run the Flask app
     
